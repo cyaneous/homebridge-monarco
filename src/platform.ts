97fa0790d76b76c83a8564c4103a00030930cf7e
lupinglade <lupinglade@cyaneous.com>
2023-11-01 12:00:37 -0700

    error handling

--- src/platform.ts
+++ src/platform.ts
@@ -106,8 +106,7 @@ export class MonarcoPlatform implements DynamicPlatformPlugin {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.kind + '_DI'+device.digitalInput+'DO'+
-        device.digitalOutput+'AI'+device.analogInput+'AO'+device.analogOutput);
+      const uuid = this.api.hap.uuid.generate(device.id);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
