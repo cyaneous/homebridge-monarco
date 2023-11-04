103b70fec1ef65e66e6b9d0ecf51e493d8c5216b
lupinglade <lupinglade@cyaneous.com>
2023-11-04 15:59:13 -0700

    test

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -253,7 +253,7 @@ export class ContactSensorAccessory {
       .onGet(this.getContactSensorState.bind(this));
 
     let tick = 0;
-    monarco.on('rx', (data) => {
+    monarco.on('xrx', (data) => {
       tick++;
 
       if(tick % 32 === 0) {
