2a5aca3726cb3ccdcb8667c98c04144d25f49ebb
lupinglade <lupinglade@cyaneous.com>
2023-11-06 17:57:51 -0800

    remove stale accessories

--- src/platform.ts
+++ src/platform.ts
@@ -156,7 +156,7 @@ export class MonarcoPlatform implements DynamicPlatformPlugin {
 
     // remove any stale accessories
     for (const accessory of this.accessories) {
-      if (config.devices.find(device => this.api.hap.uuid.generate(device.id) === accessory.UUID) === null) {
+      if (!config.devices.some(device => this.api.hap.uuid.generate(device.id) === accessory.UUID)) {
         this.log.info('Removing existing accessory from cache:', accessory.displayName);
         this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
